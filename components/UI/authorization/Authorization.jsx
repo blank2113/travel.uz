5dc808d8629b5a3211f7885ed349d993de7fd622
blank2113 <hollywoodthere@gmail.com>
2023-02-07 04:16:02 +0500

    need to add UI material

--- components/UI/authorization/Authorization.jsx
+++ components/UI/authorization/Authorization.jsx
@@ -5,57 +5,21 @@ import { useDispatch } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Authorization = () => {
   const dispatch = useDispatch();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [emailDirty, setEmailDirty] = useState(false);
-  const [passwordDirty, setPasswordDirty] = useState(false);
-  const [emailError, setEmailError] = useState("Email не может быть пустым");
-  const [passwordError, setPasswordError] = useState(
-    "Пароль не может быть пустым"
-  );
-const [formValid, setFormValid] = useState(false)    
-
-    useEffect(()=>{
-        if(emailError || passwordError){
-            setFormValid(false)
-        }else{
-            setFormValid(true)
-        }
-    },[emailError,passwordError])
-  const emailHandler = (e) => {
-    setEmail(e.target.value);
-    const re =
-      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    if (!re.test(String(e.target.value).toLowerCase())) {
-      setEmailError("Некорректный Email");
-    } else {
-      setEmailError("");
-    }
-  };
-
-  const passwordHandler = (e) => {
-    setPassword(e.target.value);
-    if (e.target.value.length < 3 ) {
-      setPasswordError("Пароль должень содержать больше 3");
-      if(!e.target.value){
-        setPasswordError("Пароль не может быть пустым");
-      }
-    } else {
-        setPasswordError("");
-    }
-  };
-
-  const blurHandler = (e) => {
-    switch (e.target.name) {
-      case "email":
-        setEmailDirty(true);
-      case "password":
-        setPasswordDirty(true);
-        break;
-    }
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+    reset,
+  } = useForm({
+    mode: "onBlur"
+  });
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+    reset()
   };
 
   return (
@@ -63,8 +27,7 @@ const [formValid, setFormValid] = useState(false)
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className={classes.Authorization}
-    >
+      className={classes.Authorization}>
       <div className={classes.inner}>
         <div className={classes.top}>
           <FontAwesomeIcon
@@ -73,30 +36,33 @@ const [formValid, setFormValid] = useState(false)
           />
           <p className={classes.title}>Пожалуйста, авторизуйтесь:</p>
         </div>
-        <form>
-          <input
-            value={email}
-            onBlur={(e) => blurHandler(e)}
-            onChange={(e) => emailHandler(e)}
-            name="email"
-            type="text"
-            placeholder="Email"
-          />
-          {emailDirty && emailError && (
-            <div style={{ color: "red" }}>{emailError}</div>
-          )}
-          <input
-            value={password}
-            onBlur={(e) => blurHandler(e)}
-            onChange={(e) => passwordHandler(e)}
-            name="password"
-            type="password"
-            placeholder="Пароль"
-          />
-          {passwordDirty && passwordError && (
-            <div style={{ color: "red" }}>{passwordError}</div>
-          )}
-          <button disabled={!formValid} type="submit">Войти</button>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label>
+            <input
+              type="email"
+              {...register("email", {
+                required: "Поле обязательно к заполнению!",
+                pattern:/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+              })}
+              placeholder="Email"
+            />
+          </label>
+          <div style={{ height: 40 }}>
+            {errors?.email && <p>{errors?.email.message || "Error!"}</p>}
+          </div>
+          <label>
+            <input type="password" {...register("password",{
+              required:"Поле обязательно к заполнению!",
+              minLength :{
+                value:8,
+                message: "Минимум 8 символов"
+              }
+            })}/>
+          </label>
+          <div style={{ height: 40 }}>
+            {errors?.password && <p>{errors?.password.message || "Error!"}</p>}
+          </div>
+          <input type="submit" disabled={!isValid}/>
         </form>
       </div>
     </motion.div>
